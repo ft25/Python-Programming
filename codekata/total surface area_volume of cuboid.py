78cd2cec512d3e6a4d67a67c2dc8befa190d7ec6
ft25 <37336785+ft25@users.noreply.github.com>
2018-04-27 10:40:42 +0530

    Update total surface area_volume of cuboid.py

--- codekata/total surface area_volume of cuboid.py
+++ codekata/total surface area_volume of cuboid.py
@@ -2,6 +2,6 @@ length=int(input("enter length:"))
 width=int(input("enter width:"))
 height=int(input("enter height:"))
 tsa=(2*(length*width+length*height+width*height))
-vol=length*width*height
+vol=(length*width*height)
 print(tsa)
 print(vol)
